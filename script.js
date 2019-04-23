6e71cb70a2c7a9fc699f028243b816855ed53d54
Marshall <taengoo890309@naver.com>
2019-04-23 17:43:11 +0900

    upload

---
+++ script.js
@@ -0,0 +1,39 @@
+document.getElementById("psbtn").addEventListener("click", function () {
+    const pwoutput = document.getElementById("password");
+
+    let password = "",
+        char,
+        chars = "";
+
+    if (document.getElementById("number").checked) {
+        chars = chars + "0123456789"
+    }
+    if (document.getElementById("symbol").checked) {
+        chars = chars + "!@#$%^&*()_+-=`~[]{};:'\",.<>/\\|"
+    }
+    if (document.getElementById("capital").checked) {
+        chars = chars + "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    }
+    if (document.getElementById("small").checked) {
+        chars = chars + "abcdefghijklmnopqrstuvwxyz"
+    }
+    if (document.getElementById("rs").checked) {
+        chars = chars.replace(/01|i|O/g, "")
+    }
+
+    for (let i = 0; i < document.getElementById("pwlength").value; i++) {
+        const random = Math.floor(Math.random() * chars.length);
+        char = chars.charAt(random);
+        password = password + char
+    }
+
+    pwoutput.innerText = password;
+
+    if (document.getElementById("as").checked) {
+        const select = document.createRange()
+        select.selectNode(pwoutput),
+            window.getSelection().removeAllRanges(),
+            window.getSelection().addRange(select),
+            document.execCommand("copy");
+    }
+})
